8d876d8e365599f90ca4fc0002ce4cf0ef9c4483
maxim.ananyev <maksim.ve@gmail.com>
2026-09-03 12:37:19 +0300

    Добавит компоненты страниц Login, Favorites, Offer и NotFound

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <div className="page">
+      <h1>404. Page not found</h1>
+      <a href="/">Go to main page</a>
+    </div>
+  );
+}
+
+export default NotFoundPage;
