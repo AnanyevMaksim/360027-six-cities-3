f8d3c7b82559f21519760823abe58c31351d8e13
maxim.ananyev <maksim.ve@gmail.com>
2026-09-15 15:47:05 +0300

    Вынесет создание роутера за пределы компонента App

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { AppRoute, AuthorizationStatus } from '@/const';
+import { AppRoute, AuthorizationStatus, Setting } from '@/const';
 import MainPage from '@/pages/main-page/main-page';
 import LoginPage from '@/pages/login-page/login-page';
 import FavoritesPage from '@/pages/favorites-page/favorites-page';
@@ -7,38 +7,34 @@ import OfferPage from '@/pages/offer-page/offer-page';
 import NotFoundPage from '@/pages/not-found-page/not-found-page';
 import PrivateRoute from '@/components/private-route/private-route';
 
-type AppProps = {
-  placesCount: number;
-}
+const router = createBrowserRouter([
+  {
+    path: AppRoute.Root,
+    element: <MainPage placesCount={Setting.PlacesCount} />,
+  },
+  {
+    path: AppRoute.Login,
+    element: <LoginPage />,
+  },
+  {
+    path: AppRoute.Favorites,
+    element: (
+      <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+        <FavoritesPage />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: AppRoute.Offer,
+    element: <OfferPage />,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+]);
 
-function App({ placesCount }: AppProps): JSX.Element {
-  const router = createBrowserRouter([
-    {
-      path: AppRoute.Root,
-      element: <MainPage placesCount={placesCount} />,
-    },
-    {
-      path: AppRoute.Login,
-      element: <LoginPage />,
-    },
-    {
-      path: AppRoute.Favorites,
-      element: (
-        <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
-          <FavoritesPage />
-        </PrivateRoute>
-      ),
-    },
-    {
-      path: AppRoute.Offer,
-      element: <OfferPage />,
-    },
-    {
-      path: '*',
-      element: <NotFoundPage />,
-    },
-  ]);
-
+function App(): JSX.Element {
   return (
     <RouterProvider router={router} />
   );
